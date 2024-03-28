1a64fa4543e381e08f65122dd7109a76ceea1cc6
Alisaeed022 <165067576+Alisaeed022@users.noreply.github.com>
2024-03-28 20:58:49 +0500

    Add files via upload

---
+++ bb.js
@@ -0,0 +1,36 @@
+/*console.log("Starting");
+function handleBindings(){
+console.log("Handle Bindings");}
+
+function doBindings(){
+console.log("Do Bindings");
+var btn= document.getElementById("btAdd");
+btn.onclick=handleBindings; //passing function ref
+}
+
+window.onload = doBindings; 
+console.log("Finished");*/
+
+
+
+window.onload = function(){
+    var btn = document.getElementById("btnAdd")
+    btn.onclick = AddToDo;
+};
+
+function AddToDo(){
+    var myinput = document.getElementById("myinput").value;
+
+    var todos = document.getElementById("todos");
+    var newtextnode = document.createTextNode(myinput);
+    var newLi = document.createElement("li")
+    newLi.appendChild(newtextnode);
+    todos.appendChild(newLi);
+}
+
+
+function handleDelete(e){
+    var tag = e.target;
+    var li= tag.parentNode;
+    li.parentNode.removeChild(li)
+}
